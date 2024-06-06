7affa696144118d02712587807be00128759357d
Alex Andrango <arandrangoa@uce.edu.ec>
2024-06-05 19:52:01 -0500

    Taller 16 Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,18 @@
+<!--dentro del template va a estar programado todo lo relacionado al elemento html-->
+<template>
+  <h2>Contador</h2>
+  <p>100</p>
+</template>
+
+<!--Sentro del scrip se debe progrmar todo lo relacionado a javascrip 
+//va a estar todas las opciones de optionsapia-->
+<script>
+export default {
+
+}
+</script>
+
+<!--Aqui va a estar todos los estilos del componente-->
+<style>
+
+</style>
